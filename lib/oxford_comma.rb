196c12a06227b2aea0ed5b96ea98dc9783e9f3bb
Canino Developments <ian.canino@gmail.com>
2018-04-14 00:10:13 -0400

    implemented oxford comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
-
+def oxford_comma(anArray)
+  astring = anArray[0].to_s
+  case anArray.length
+    when 1
+      return anArray[0]
+    when 2
+      astring = astring + " and #{anArray[1]}"
+    else
+      iteration = 1
+      while iteration < anArray.length
+        astring = astring + ", "
+        if iteration == anArray.length - 1
+          astring = astring + "and "
+        end
+      astring = astring + anArray[iteration].to_s
+      iteration += 1
+      end
+  end
+  return astring
 end
